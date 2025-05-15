afbd3afb541e12fdfcd59db219fbb2c5d1018ce1
심민 <tlaals44@naver.com>
2025-05-16 02:36:27 +0900

    추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="6jtaUiaRpd1H0P9bCa6VSNrOV65nlhuWIgOQhYh2sYk"
+        />
+      </Head>
       <body className="font-normal break-keep selection:bg-PRIMARY_LIGHT selection:dark:text-BLACK text-BLACK dark:bg-BLACK dark:text-white">
         <Main />
         <NextScript />
