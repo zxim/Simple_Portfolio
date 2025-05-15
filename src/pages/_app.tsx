4e23ace93a01cdf78b848b03430e32a76e1e8270
심민 <tlaals44@naver.com>
2025-05-16 02:29:02 +0900

    vercel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,26 +2,30 @@ import "@/styles/globals.css";
 import { DefaultSeo } from "next-seo";
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
+import Script from "next/script";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
-/**
- * @description SEO를 위해 본인의 정보로 수정해주세요.
- */
+import * as gtag from "../lib/gtag";
+
+const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS;
+
 const DEFAULT_SEO = {
   title: "심민 | Front-End Dev",
-  description: "안녕하세요, 프론트엔드 개발자 홍길동입니다.",
-  canonical: "https://www.naver.com/",
+  description: "안녕하세요, 프론트엔드 개발자 심민입니다.",
+  canonical: "https://zxim.vercel.app/",
   openGraph: {
     type: "website",
     locale: "ko_KR",
-    url: "https://www.naver.com/",
-    title: "홍길동 | Front-End Dev",
-    site_name: "홍길동 | Front-End Dev",
+    url: "https://zxim.vercel.app/",
+    title: "심민 | Front-End Dev",
+    site_name: "심민 | Front-End Dev",
     images: [
       {
         url: "/share.png",
         width: 285,
         height: 167,
-        alt: "홍길동 | Front-End Dev",
+        alt: "심민 | Front-End Dev",
       },
     ],
   },
@@ -34,11 +38,11 @@ const DEFAULT_SEO = {
   additionalMetaTags: [
     {
       name: "application-name",
-      content: "홍길동 | Front-End Dev",
+      content: "심민 | Front-End Dev",
     },
     {
       name: "msapplication-tooltip",
-      content: "홍길동 | Front-End Dev",
+      content: "심민 | Front-End Dev",
     },
     {
       name: "viewport",
@@ -48,8 +52,42 @@ const DEFAULT_SEO = {
 };
 
 const App = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    router.events.on("hashChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+      router.events.off("hashChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
+      {/* ✅ GA 스크립트 삽입 */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+
       <DefaultSeo {...DEFAULT_SEO} />
       <ThemeProvider attribute="class">
         <Component {...pageProps} />
